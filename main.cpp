1751173cf6c80cf9bcbb466dcbff513fc7384afc
Said Ammaev <ammsaid@mail.ru>
2018-07-15 02:35:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Clothes-color
+//
+//  Created by Саид Аммаев on 15.07.2018.
+//  Copyright © 2018 Саид Аммаев. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, const char * argv[]) {
+    
+    return EXIT_SUCCESS;
+}
